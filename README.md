94bb9354deac3189e13ec53e0974661aa4d3b433
Philippe Charrière <ph.charriere@gmail.com>
2017-08-19 10:08:44 +0200

    first version of the backend

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # pico-backend-demo
+
+The [**pico**](https://github.com/k33g/pico) library has been installed with thes command:
+
+```shell
+curl -o pico.js https://raw.githubusercontent.com/k33g/pico/master/index.js
+```
+but you can use 
+```shell
+npm install git+https://git@github.com/k33g/pico.git --save
+```
+
+> I only want to try to improve the delay of deplyment
+
---
+++ index.js
@@ -0,0 +1,18 @@
+const {DiscoveryBackendServer} = require('./pico')
+
+/**
+ * On the Clever Cloud platform
+ * http port have to be 8080
+ * and you can reach the service with the 80 http port
+ */
+
+let port = process.env.PORT || 9099;
+
+let backend = new DiscoveryBackendServer()
+
+backend.start({port: port}, res => {
+  res.when({
+    Failure: error => console.log("😡 Houston? We have a problem!"),
+    Success: port => console.log(`🌍 pico discovery backend server is started on ${port}`)
+  })
+})
