d19403515f0b794dd4263470fa17abb6614b62f7
Philippe Charrière <ph.charriere@gmail.com>
2017-08-20 11:17:56 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {DiscoveryBackendServer, Client} = require('./pico')
+const {DiscoveryBackendServer, Client, Wrapper} = require('./pico')
 
 /**
  * On the Clever Cloud platform
@@ -9,6 +9,11 @@ const {DiscoveryBackendServer, Client} = require('./pico')
 let port = process.env.PORT || 9099;
 
 let backend = new DiscoveryBackendServer()
+
+class Down extends Wrapper {}
+class Up extends Wrapper {}
+
+let check = service => service.status=="DOWN" ? Down.of(service) : Up.of(service)
 
 backend.start({port: port}, res => {
   res.when({
@@ -21,6 +26,10 @@ backend.start({port: port}, res => {
           Failure: failure => console.log(failure.error, failure.service),
           Success: service => {
             console.log(`👋 🍄 this service ${service.registration} is ${service.status}`)
+            check(service).when({
+              Down: service => console.log(`🍄 this service ${service.registration} is DOWN, you can remove it of the list`),
+              Up: service => console.log(`👋 this service ${service.registration} is UP`)
+            })
           }
         })
       }})
@@ -28,77 +37,3 @@ backend.start({port: port}, res => {
     }
   })
 })
-
-/*
-
-      function updateStatusOfServices() {
-        for(var keyServices in backend.servicesDirectory) { 
-          console.log(keyServices, ":") 
-          backend.servicesDirectory[keyServices].forEach(service => {
-            let client = new Client({service: service})
-            client.healthCheck()
-              .then(serviceHealth => {
-                if(serviceHealth.status=="DOWN") {
-                  console.log(`👎 The service with the id ${serviceHealth.registration} is marked to "DOWN"`)
-                  console.log(`It probably belongs to a stopped/removed VM or container`)
-                } else {
-                  console.log(`👍 The service with the id ${serviceHealth.registration} is marked to "UP"`)
-                }
-                // updating the status in the services lis
-                service.status = serviceHealth.status
-              })
-          })
-        }
-      } // end function updateStatusOfServices()
-      setInterval(updateStatusOfServices, 5000);
-
-
-
-  // remove phantom services
-  watchServiceList({interval}) {
-    let servicesList = this.servicesDirectory
-    function clean() {
-      //var d = new Date();
-      for(var keyServices in servicesList) { 
-        console.log(keyServices) 
-        console.log(" -", servicesList[keyServices])
-        servicesList[keyServices].forEach(service => {
-          let client = new Client({service: service})
-          client.healthCheck()
-            .then(data => {
-              console.log("  =>", data)
-              // if data.registration <> service.registration => remove this service
-              if(data.registration!==service.registration) {
-                let index = servicesList[keyServices].indexOf(service)
-                if (index > -1) {
-                  servicesList[keyServices].splice(index, 1)
-                }
-              }
-            })
-            .catch(err => {
-              //TODO
-            })
-
-        })
-      }
-    }
-
-    let t = setInterval(clean, interval);
-    
-
-  }
-*/
-
-
-/*
-    this.service.post({uri:`/api/clean/services`, f: (request, response) => {
-      let data = request.body
-      let domain = data.domain
-      let name = data.name
-
-      if(this.servicesDirectory[data.keyServices]) {
-        let serviceObject = this.servicesDirectory[keyServices].filter(item=>item.name==data.name && item.domain==data.domain)
-      } 
-      response.sendJson({registration: data.record.registration})
-    }})
-*/
